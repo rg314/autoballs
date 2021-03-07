0d11666d74a4f94de1ea6937ac9586119f4400df
rg314 <ryan.greenhalgh@hotmail.co.uk>
2021-03-07 18:21:48 +0000

    reqs

--- scripts/get_fiji_version.sh
+++ scripts/get_fiji_version.sh
@@ -1,25 +1,39 @@
 #!/bin/sh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  FIJIPATH='https://docs.google.com/uc?export=download&id=1KxbVQIsav-jZeFsXx0D8XmJZlYu-8k4o'
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  FIJIPATH='https://docs.google.com/uc?export=download&id=1RXXXwjBRTtiRyw5F1mhEB44U9_UyCyVA'
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-  if [ ! -d "Fiji" ]; then
-    mkdir Fiji
-  fi
-
-  cd Fiji                            && \
-
-  if [ ! -d "Fiji.app" ]; then
-      wget --no-check-certificate -r $FIJIPATH -O Fiji.app.zip  && \
-      unzip Fiji.app.zip                                  && \
-      rm Fiji.app.zip
-  fi
-
-  cd ..
-
-elif [[ "$OSTYPE" == "win32" ]]; then
-	echo 'Not implemented for windows, visit https://docs.google.com/uc?export=download&id=14X0PI0YEIUESLvCMzsRiD1vLTuo9bTyr'
+if [ ! -d "Fiji" ]; then
+  mkdir Fiji
 fi
+
+cd Fiji                            && \
+
+if [ ! -d "Fiji.app" ]; then
+    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1KxbVQIsav-jZeFsXx0D8XmJZlYu-8k4o' -O Fiji.app.zip  && \
+    unzip Fiji.app.zip                                  && \
+    rm Fiji.app.zip
+fi
+
+cd ..
+
+# elif [ "$OSTYPE" == "darwin"* ]; then
+
+#   if [ ! -d "Fiji" ]; then
+#     mkdir Fiji
+#   fi
+
+#   cd Fiji                            && \
+
+#   if [ ! -d "Fiji.app" ]; then
+#       wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1RXXXwjBRTtiRyw5F1mhEB44U9_UyCyVA' -O Fiji.app.zip  && \
+#       unzip Fiji.app.zip                                  && \
+#       rm Fiji.app.zip
+#   fi
+
+#   cd ..
+
+# elif [ "$OSTYPE" == "win32" ]; then
+# 	echo 'Not implemented for windows, visit https://docs.google.com/uc?export=download&id=14X0PI0YEIUESLvCMzsRiD1vLTuo9bTyr'
+# fi
+
 
