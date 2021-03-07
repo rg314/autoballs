e1390142b9d26980b5d1af1d2444c6fcf5a1984d
rg314 <ryan.greenhalgh@hotmail.co.uk>
2021-03-07 18:31:31 +0000

    reqs

--- scripts/get_fiji_version.sh
+++ scripts/get_fiji_version.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-if [ ! -d "Fiji" ]; then
-  mkdir Fiji
+  if [ ! -d "Fiji" ]; then
+    mkdir Fiji
+  fi
+
+  cd Fiji                            && \
+
+  if [ ! -d "Fiji.app" ]; then
+      wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1KxbVQIsav-jZeFsXx0D8XmJZlYu-8k4o' -O Fiji.app.zip  && \
+      unzip Fiji.app.zip                                  && \
+      rm Fiji.app.zip
+  fi
+
+  cd ..
+
+elif [ "$OSTYPE" == "darwin"* ]; then
+
+  if [ ! -d "Fiji" ]; then
+    mkdir Fiji
+  fi
+
+  cd Fiji                            && \
+
+  if [ ! -d "Fiji.app" ]; then
+      wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1RXXXwjBRTtiRyw5F1mhEB44U9_UyCyVA' -O Fiji.app.zip  && \
+      unzip Fiji.app.zip                                  && \
+      rm Fiji.app.zip
+  fi
+
+  cd ..
+
+elif [ "$OSTYPE" == "win32" ]; then
+	echo 'Not implemented for windows, visit https://docs.google.com/uc?export=download&id=14X0PI0YEIUESLvCMzsRiD1vLTuo9bTyr'
 fi
-
-cd Fiji                            && \
-
-if [ ! -d "Fiji.app" ]; then
-    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1KxbVQIsav-jZeFsXx0D8XmJZlYu-8k4o' -O Fiji.app.zip  && \
-    unzip Fiji.app.zip                                  && \
-    rm Fiji.app.zip
-fi
-
-cd ..
-
-# elif [ "$OSTYPE" == "darwin"* ]; then
-
-#   if [ ! -d "Fiji" ]; then
-#     mkdir Fiji
-#   fi
-
-#   cd Fiji                            && \
-
-#   if [ ! -d "Fiji.app" ]; then
-#       wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1RXXXwjBRTtiRyw5F1mhEB44U9_UyCyVA' -O Fiji.app.zip  && \
-#       unzip Fiji.app.zip                                  && \
-#       rm Fiji.app.zip
-#   fi
-
-#   cd ..
-
-# elif [ "$OSTYPE" == "win32" ]; then
-# 	echo 'Not implemented for windows, visit https://docs.google.com/uc?export=download&id=14X0PI0YEIUESLvCMzsRiD1vLTuo9bTyr'
-# fi
 
 
